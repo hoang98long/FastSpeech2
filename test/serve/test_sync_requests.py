76b095822b872608b1e6736634e550ade4c8d41f
phamlehuy53 <phamlehuy53@gmail.com>
2021-07-09 15:03:27 +0700

    serve

--- test/serve/test_sync_requests.py
+++ test/serve/test_sync_requests.py
@@ -6,19 +6,18 @@ from utils import read_data, url, log_request, headers
 import random
 from typing import Any, Dict
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 #%%
 
 # @pytest.mark.skip()
 @log_request()
 def get(payloads)-> Any:
     payloads = list(map( lambda x: json.dumps({'text': x}), payloads))
-    # print(f"Reading data")
-    print(payloads[0])
     responses = [ requests.request('GET', url, headers=headers, data=p) for p in payloads ]
     # return response.content
     return responses
-
-
 
 def test_request():
     # print('Inside test sync request')
@@ -33,11 +32,4 @@ def test_request():
     
 if __name__ == '__main__':
     pass
-    # payloads = read_data()
-    # n = len(payloads)
-    # _start = time.time()
-    # for payload in payloads:
-    #     test(payload)
-    
-    _end = time.time()
 
--- test/serve/utils.py
+++ test/serve/utils.py
@@ -3,6 +3,10 @@ import os
 from typing import List, Any
 import requests
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 #%%
 headers = {
   'Content-Type': 'application/json'
@@ -21,20 +25,20 @@ def read_data(fpath: str = os.path.join(cur_dir, './data.txt')):
 
     return data[:10]
 
-#TODO: print -> logging
+# TODO: print -> logging
 def log_request(type:str = 'sync'):
-    # print('Inside log_request')
     if type == 'sync':
         get_time = time.time
     def decorator(func: Any):
-        # print('Decorator')
         def wrapper(payloads: List[Any], *args, **kwargs):
-            print('Requesting {len(payloads)} payloads')
+            _start_mess = f'Requesting {len(payloads)} payloads'
+            logger.info(_start_mess)
             _start = get_time()
             responses= func(payloads)
             _end = get_time()
             avg_time = (_end-_start)/len(payloads)
-            print(f"Request done with {avg_time}s/payload")
+            _end_mess = f"Request done with {avg_time}s/payload"
+            logger.info(_end_mess)
             return responses
         return wrapper
     return decorator
