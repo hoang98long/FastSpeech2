61ddd18dd5d5b8571589119b123b3b843bcc4491
phamlehuy53 <phamlehuy53@gmail.com>
2021-07-13 15:03:47 +0700

    serve

--- static/home.js
+++ static/home.js
@@ -1,3 +1,43 @@
+$.ajaxTransport("+binary", function (options, originalOptions, jqXHR) {
+    // check for conditions and support for blob / arraybuffer response type
+    if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob))))) {
+        return {
+            // create new XMLHttpRequest
+            send: function (headers, callback) {
+                // setup all variables
+                var xhr = new XMLHttpRequest(),
+                    url = options.url,
+                    type = options.type,
+                    async = options.async || true,
+                    // blob or arraybuffer. Default is blob
+                    dataType = options.responseType || "blob",
+                    data = options.data || null,
+                    username = options.username || null,
+                    password = options.password || null;
+
+                xhr.addEventListener('load', function () {
+                    var data = {};
+                    data[options.dataType] = xhr.response;
+                    // make callback and send data
+                    callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+                });
+
+                xhr.open(type, url, async, username, password);
+
+                // setup custom headers
+                for (var i in headers) {
+                    xhr.setRequestHeader(i, headers[i]);
+                }
+
+                xhr.responseType = dataType;
+                xhr.send(data);
+            },
+            abort: function () {
+                jqXHR.abort();
+            }
+        };
+    }
+});
 function sendText() {
     /* var item = {
         'text': document.getElementById("fname").value
@@ -12,16 +52,25 @@ function sendText() {
         type: "POST",
         url: URL,
         data: json_item, 
-        dataType: 'json',
+        dataType: 'binary',
         contentType: "application/json; charset=utf-8",
         responseType: 'arraybuffer',
+        headers: { 'X-Requested-With': 'XMLHttpRequest' }
         success: function(response) {
             data = new Unit8Array(response);
             console.log(length(data));
             $('#audio_src'.attr('src', data))
             return data;
         }
-    });
+    }).then(
+        function(response) {
+            data = new Unit8Array(response);
+            console.log(length(data));
+            $('#audio_src'.attr('src', data))
+            return data;
+        }
+
+    );
     // var xmlHttp = new XMLHttpRequest();
     // xmlHttp.onreadystatechange = function() {
     //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
